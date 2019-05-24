955517a18ce24753f0047313a5d37d3e5e588d0c
Victor Coustenoble <victor.coustenoble@gmail.com>
2019-05-24 22:02:59 +0200

    Version 24-05-19 22:02:59

--- flow_checkin.sh
+++ flow_checkin.sh
@@ -21,9 +21,9 @@ fi
 ENDPOINT="/v4/flows/$flow_id/package"
 # GET : result is a zip file
 
-echo $CURL --user $CREDENTIALS $HOST$ENDPOINT -o flow_$flow_id.zip
+echo $CURL --user $CREDENTIALS $HOST$ENDPOINT -o ./flow_$flow_id/flow_$flow_id.zip
 
-output=$( $CURL --user $CREDENTIALS $HOST$ENDPOINT -o flow_$flow_id.zip)
+output=$( $CURL --user $CREDENTIALS $HOST$ENDPOINT -o ./flow_$flow_id/flow_$flow_id.zip)
 
 #output=$( $CURL -sw '%{http_code}' --user $CREDENTIALS $HOST$ENDPOINT)
 #response=$(curl -L -s -o $file -z $file $url -w "%{http_code}")
@@ -38,6 +38,6 @@ output=$( $CURL --user $CREDENTIALS $HOST$ENDPOINT -o flow_$flow_id.zip)
 
 # -------------  Commit and Push in Github  ---------------
 
-git add flow_$flow_id.zip
+git add /flow_$flow_id./flow_$flow_id.zip
 git commit -m "Version $time"
 git push
